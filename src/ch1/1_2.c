b8e14a9108f17e182ce7227c1fa2d19a2e422a1e
Artem Harutyunyan <artem@harutyunyan.am>
2013-06-10 23:02:09 -0700

    ADD: 1_2, 1_3 and 1_4

---
+++ src/ch1/1_2.c
@@ -0,0 +1,71 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
+/**
+* @file   1_2.c
+* @author Artem Harutyunyan (artem_at_harutyunyan.am)
+* @brief  Excercise 1.2 (Cracking the Coding Interview)
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT_SIZE 1024
+
+/** @brief reverse a null-terminated string */
+void reverse(char *input) {
+  unsigned int i = 0;
+  const unsigned int last = strlen(input) - 1;
+  char c;
+
+  while (i < last - i) {
+    c = input[last - i];
+    input[last - i] = input[i];
+    input[i] = c;
+    ++i;
+  }
+}
+
+int main() {
+
+  char input[MAX_INPUT_SIZE];
+
+  /* Read the string */
+  printf("Please enter the string: ");
+  fgets(input, MAX_INPUT_SIZE, stdin);
+
+  /* get rid of new line */
+  input[strlen(input) - 1] = '\0';
+
+  reverse(input);
+
+  /* Skip the newline */
+  printf("The reversed string is: %s\n", input);
+
+  return 0;
+}
+
